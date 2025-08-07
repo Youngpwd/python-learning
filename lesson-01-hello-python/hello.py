1d01f82002ccbfbfdecf04ed4c0909d41535073d
Charles <cbyoung415@gmail.com>
2025-08-07 10:43:11 -0700

    Add Lesson 1 - Hello Python

---
+++ lesson-01-hello-python/hello.py
@@ -0,0 +1,12 @@
+#printing message
+print("Hello, Im learning python on my own!!!!!")
+
+#variables
+name = "Brad"
+age = 28
+language ="python"
+
+#printing variables
+print("My name is", name)
+print("I am", age, "years old.")
+print("I am self learning", language)
